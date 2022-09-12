054e4206950b663bd326a3643f5f90d78e99af70
Ibnul Huq <ibnul.huq@gmail.com>
2022-09-12 09:31:39 +0100

    push front-end to seperate repo

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: { ignoreBuildErrors: true },
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
